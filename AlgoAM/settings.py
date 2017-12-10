aa07a4fd22cf090583adbdac67fae1da298891a9
anamah <mmakhneva@icloud.com>
2017-12-10 14:48:05 +0300

    Google Chart Part One

--- AlgoAM/settings.py
+++ AlgoAM/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'AlgoAMDjango',
-    'AlgoAM',
+
 ]
 
 MIDDLEWARE_CLASSES = [
--- AlgoAM/urls.py
+++ AlgoAM/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from .views import HomeView, get_data
+
 urlpatterns = [
+    url(r'^$/', HomeView.as_view(), name='home'),
+    url(r'^api/data/$', get_data, name='api-data'),
     url(r'^admin/', admin.site.urls),
 ]
